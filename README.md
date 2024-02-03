50251dd9d529dc09377a27ead8de96cbd0bccf07
pmpm2000 <patryk1705@gmail.com>
2024-02-03 15:15:56 +0100

    grepolympia, olympus essentials integration

--- README.md
+++ README.md
@@ -1,5 +1,66 @@
-Bot do Discorda pomagajacy w obliczeniach w grze Grepolis.
-## CZAS OCHRONY NA SWIECIE Z PREDKOSCIA 5
+# English
+A bot for Discord that helps with calculations and automates processes in the Grepolis game.
+## Functions
+### INVITATIONS VIA DISCORD
+Requires Olympus Essentials setup (https://github.com/pmpm2000/olympus_essentials).
+Calling the bot using the "zap" command. The first argument is the alliance number, and the second is the nickname of the player to be invited.
+Examples:
+  - zap 1 gracz123
+  - zap 3 pmpm2000
+
+### GREPOLYMPIA
+1. Command starts with "lvl"
+2. After the space, enter your athlete's level.
+3. You will get the best stat distributions for each Grepolympia discipline. At high levels, you have to wait a few seconds for the result, because of high computational complexity.
+4. Examples:
+   - lvl 122
+   - lvl 8
+
+### CITY PROTECTION TIME AT 5X WORLDS
+1. The bot is invoked with the "ochr" command and time the protection was cast. It gives the times of next three spells to be cast.
+2. Times can be entered with or without seconds - in the latter case, the bot will assume that a second is 00.
+3. Midnight can be entered either as 24, 0 or 00.
+4. The time is entered using colons, just like in the game, so copying the time from the report will work.
+5. Examples:
+     - ochr 23:16:22
+     - ochr 00:45
+     - ochr 24:01:19
+
+### TRANSPORT SHIPS CALCULATOR
+1. The bot is invoked with the "tra" command and appropriate arguments. Calculations are for speed transport ships with Bunks research.
+2. There are 2 ways to specify arguments:
+    - number of free citizens only - a good way if we have a balance in land units and transport ships, e.g. when the city is empty.
+    - number of free citizens, number of transport ships, number of land units - more advanced way, good when we already have some troops recruited.
+3. The bot will return number of fast transport ships that is needed.
+4. Examples:
+    - tra 3883 => we have 3883 free citizens and the city is empty or there is already a balance between units
+    - tra 1214 30 269 => we have 1214 free citizens, but also 30 transport ships and 269 land units recruited
+
+## Configuration
+1. In the code, change the value of the 'token' variable to your bot's token.
+2. Change the value of the 'webhook_url' variable to the url to your webhook.
+3. If you want to use the invitations, permissions and alerts features, configure and run Olympus Essentials (https://github.com/pmpm2000/olympus_essentials). The standard server port is 8002.
+4. Enjoy.
+
+# Polski
+Bot do Discorda pomagajacy w obliczeniach i automatyzujący procesy w grze Grepolis.
+## Funkcje
+### ZAPROSZENIA PRZEZ DISCORDA
+Wymaga skonfigurowania Olympus Essentials (https://github.com/pmpm2000/olympus_essentials).
+Wywołanie bota za pomocą polecenia "zap". Pierwszym argumentem jest numer sojuszu, a drugim jest nick gracza, który ma być zaproszony.
+Przykład:
+  - zap 1 gracz123
+  - zap 3 pmpm2000
+
+### GREPOLYMPIA
+1. Polecenie rozpoczyna się od "lvl"
+2. Po spacji wpisz poziom swojego atlety.
+3. Otrzymasz najlepsze rozkłady statystyk dla każdej konkurencji Grepolympii. Przy wysokich poziomach na wynik trzeba chwilę poczekać (~kilkanaście sekund), bo obliczenia są spore.
+4. Przykłady:
+  - lvl 122
+  - lvl 8
+
+### CZAS OCHRONY NA SWIECIE Z PREDKOSCIA 5
 1. Bota wywoluje sie komenda "ochr" i czasem rzucenia ochrony. Podaje on czasy trzech kolejnych ochronek do rzucenia.
 2. Czasy mozna wpisywac z sekundami lub bez - w drugim przypadku bot zalozy, ze sekunda to 00.
 3. Polnoc mozna wpisac zarowno jako 24, 0 jak i 00.
@@ -9,8 +70,8 @@ Bot do Discorda pomagajacy w obliczeniach w grze Grepolis.
     - ochr 00:45
     - ochr 24:01:19
 
-## LICZBA TRANSOW W MIESCIE
-1. Bota wywoluje sie komenda "tra" i odpowiednimi argumentami
+### LICZBA TRANSOW W MIESCIE
+1. Bota wywoluje sie komenda "tra" i odpowiednimi argumentami. Obliczenia dotyczą szybkich transów z kojami.
 2. Sa dwa sposoby wpisywania argumentow:
     - tylko liczba wolnych mieszkancow - sposob dobry kiedy mamy aktualnie rownowage pomiedzy transami a ladem, czyli np. kiedy miasto jest puste.
     - liczba wolnych mieszkancow, aktualna liczba transow, aktualna liczba ladu - dobre kiedy mamy juz zbudowane po troche wszystkiego i nie chce nam sie nic przeliczac.
@@ -21,7 +82,7 @@ Bot do Discorda pomagajacy w obliczeniach w grze Grepolis.
     - tra 1214 30 269 => w miescie jest zbudowanych 30 transow i 269 wojska ladowego (uwzgledniajac te w kolejce), a oprocz tego jest jeszcze wolnych 1214 ludzi
 Wskazowka: przez "rownowage" rozumiemy tu sytuacje, kiedy transow jest odpowiednio duzo do posiadanego wojska, czyli np. jest w miescie 1 trans i 16 wojska ladowego. Drugi sposob jest bardziej uniwersalny, ale wymaga wiecej informacji.
 
-## EVENT DIONIZJE GRUDZIEN 2022
+### EVENT DIONIZJE GRUDZIEN 2022
 Pierwsze co trzeba wpisac to "ev" - znak dla bota\
 Po spacji wpisuje sie numer jednostki z tego paska jednostek na ktorym mozna je kupowac. Dla jasnosci:\
 1 - Panes\
@@ -36,3 +97,9 @@ Przyklady:\
 ev 1 15 osly\
 ev 5 21 kozy\
 ev 4 5 pędzle
+
+## Konfiguracja
+1. W kodzie zmien wartosc zmiennej 'token' na token swojego bota.
+2. Zmien wartosc zmiennej 'webhook_url' na url do swojego webhooka.
+3. Jesli chcesz korzystac z funkcji zaproszen, uprawnien i alarmow, skonfiguruj i uruchom Olympus Essentials (https://github.com/pmpm2000/olympus_essentials). Standardowy port serwera to 8002.
+4. Korzystaj
--- main.py
+++ main.py
@@ -1,23 +1,219 @@
-import datetime
 import math
+import threading
+import datetime
 import discord
-from discord.ext import commands
+import requests
+from flask import Flask, request
+
+webhook_url = 'set your webhook url'
+bot_token = 'set your token'
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 intents.presences = True
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), intents=intents, description="123")
+
+client = discord.Client(intents=intents)
+perms = [[], [], [], [], []]
+invs = [[], [], [], [], []]
+webhooks_to_send = []
+already_alarmed = []
+app = Flask(__name__)
+printlock = threading.Lock()
+queuelock = threading.Lock()
+
+base_score = [500, 150, 50, 20]
+score_matrix = [{
+        "first_skill_points": {
+            "multiplier": 15,
+            "power": 0.9
+        },
+        "second_skill_points": {
+            "multiplier": 25,
+            "power": 0.6
+        },
+        "third_skill_points": {
+            "multiplier": 20,
+            "power": 0.7
+        }
+},
+    {
+        "first_skill_points": {
+            "multiplier": 80,
+            "power": 0.7
+        },
+        "second_skill_points": {
+            "multiplier": 60,
+            "power": 0.85
+        },
+        "third_skill_points": {
+            "multiplier": 100,
+            "power": 0.5
+        }
+    },
+    {
+        "first_skill_points": {
+            "multiplier": 1.3,
+            "power": 0.75
+        },
+        "second_skill_points": {
+            "multiplier": 1.8,
+            "power": 0.6
+        },
+        "third_skill_points": {
+            "multiplier": 0.8,
+            "power": 0.95
+        }
+    },
+    {
+        "first_skill_points": {
+            "multiplier": 0.6,
+            "power": 0.95
+        },
+        "second_skill_points": {
+            "multiplier": 1.8,
+            "power": 0.6
+        },
+        "third_skill_points": {
+            "multiplier": 1.1,
+            "power": 0.8
+        }
+    }]
 
 
-@bot.event
+def get_values_perms_invs(x):
+    queuelock.acquire()
+    ret = {
+        "perms": perms[x].copy(),
+        "invites": invs[x].copy()
+    }
+    queuelock.release()
+    safe_print("Got alliance" + str(x+1) + " request. Sending " + str(ret))
+    queuelock.acquire()
+    perms[x].clear()
+    invs[x].clear()
+    queuelock.release()
+    return ret
+
+
+@app.route('/alliance1', methods=['GET'])
+def alliance1():
+    return get_values_perms_invs(0)
+
+
+@app.route('/alliance2', methods=['GET'])
+def alliance2():
+    return get_values_perms_invs(1)
+
+
+@app.route('/alliance3', methods=['GET'])
+def alliance3():
+    return get_values_perms_invs(2)
+
+
+@app.route('/alliance4', methods=['GET'])
+def alliance4():
+    return get_values_perms_invs(3)
+
+
+@app.route('/alliance5', methods=['GET'])
+def alliance5():
+    return get_values_perms_invs(4)
+
+
+@app.route('/attack', methods=['POST'])
+def attack():
+    # send_webhook(request.data.decode('utf-8'))
+    mov_id = request.get_json()["id"]
+    mess = request.get_json()["message"]
+    if mov_id not in already_alarmed:
+        already_alarmed.append(mov_id)
+        send_webhook(mess)
+    return "OK"
+
+
+def send_webhook(text):
+    safe_print("To send: " + text)
+    payload = {
+        'content': text + ' @everyone',
+        'username': 'Informator',
+        'avatar_url': 'https://i.imgur.com/wuowbcp.png',
+        'tts': True
+    }
+    try:
+        response = requests.post(webhook_url, json=payload)
+        response.raise_for_status()
+        safe_print('Data sent')
+    except requests.exceptions.RequestException as e:
+        safe_print(f'Error: {e}')
+
+
+def safe_print(message):
+    printlock.acquire()
+    print(message)
+    printlock.release()
+
+
+@client.event
 async def on_ready():
-    print("Ready")
+    safe_print('Logged as {0.user}'.format(client))
 
 
-@bot.event
-async def on_message(message: discord.Message):
-    if message.content.lower().startswith("ev "):
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith('upki '):
+        if message.author not in message.guild.roles[len(message.guild.roles)-1].members and message.author not in message.guild.roles[len(message.guild.roles)-2].members:
+            await message.channel.send("Brak dostepu do tej komendy")
+            return
+        parts = message.content.split(" ", 2)
+        if len(parts) != 3:
+            await message.channel.send("Nieprawidlowa liczba argumentow")
+            return
+        if parts[1] not in ["1", "2", "3", "4"]:  # dopisac obsluge wyjatku
+            await message.channel.send("Nieprawidlowy numer sojuszu")
+            return
+        alliance_number = int(parts[1])
+        queuelock.acquire()
+        perms[alliance_number - 1].append(parts[2])
+        queuelock.release()
+        safe_print('Added ' + parts[2] + ' to leadership ' + parts[1] + '. Queue: ' + str(perms))
+        await message.channel.send("Zarejestrowano. Nadanie uprawnien moze potrwac kilka minut.")
+        log_to_file(message.author.name + ": " + message.content)
+
+    elif message.content.lower().startswith('zap '):
+        parts = message.content.split(" ", 2)
+        if len(parts) != 3:
+            await message.channel.send("Nieprawidlowa liczba argumentow")
+            return
+        if parts[1] not in ["1", "2", "3", "4", "5"]:  # dopisac obsluge wyjatku
+            await message.channel.send("Nieprawidlowy numer sojuszu")
+            return
+        alliance_number = int(parts[1])
+        queuelock.acquire()
+        invs[alliance_number - 1].append(parts[2])
+        queuelock.release()
+        safe_print('Added ' + parts[2] + ' to invite ' + parts[1] + '. Queue: ' + str(invs))
+        await message.channel.send("Zarejestrowano. Wyslanie zaproszenia moze potrwac kilka minut.")
+        log_to_file(message.author.name + ": " + message.content)
+
+    elif message.content.lower().startswith('lvl '):
+        parts = message.content.split(" ")
+        if len(parts) != 2:
+            await message.channel.send("Nieprawidlowa liczba argumentow")
+            return
+        try:
+            val = int(parts[1])
+        except:
+            await message.channel.send("Nieprawidlowy poziom")
+            return
+        if val > 350 or val < 1:
+            await message.channel.send("Zbyt wysoki/niski poziom")
+            return
+        dist = test_skill_distribution(val)
+        to_send = "Wyniki dla " + message.content + "\nKonkurencja 1: " + str(dist[0]["skills"][0]) + "/" + str(dist[0]["skills"][1]) + "/" + str(dist[0]["skills"][2]) + ": " + str(math.floor(dist[0]["score"])) + "\nKonkurencja 2: " + str(dist[1]["skills"][0]) + "/" + str(dist[1]["skills"][1]) + "/" + str(dist[1]["skills"][2]) + ": " + str(math.floor(dist[1]["score"])) + "\nKonkurencja 3: " + str(dist[2]["skills"][0]) + "/" + str(dist[2]["skills"][1]) + "/" + str(dist[2]["skills"][2]) + ": " + str(math.floor(dist[2]["score"])) + "\nKonkurencja 4: " + str(dist[3]["skills"][0]) + "/" + str(dist[3]["skills"][1]) + "/" + str(dist[3]["skills"][2]) + ": " + str(math.floor(dist[3]["score"]))
+        await message.channel.send(to_send)
+
+    elif message.content.lower().startswith("ev "):
         msg = message.content
         args = msg.split()
         if len(args) != 4:
@@ -30,22 +226,22 @@ async def on_message(message: discord.Message):
         arg2 = args[2]
         arg3 = args[3]
         if arg1 == '1':
-            k=2
+            k = 2
             jedn1 = 'Panes'
         elif arg1 == '2':
-            k=3
+            k = 3
             jedn1 = 'Sileny'
         elif arg1 == '3':
-            k=5
+            k = 5
             jedn1 = 'Fallofor'
         elif arg1 == '4':
-            k=2
+            k = 2
             jedn1 = 'Kanephoroi'
         elif arg1 == '5':
-            k=3
+            k = 3
             jedn1 = 'Driady'
         elif arg1 == '6':
-            k=6
+            k = 6
             jedn1 = 'Arcykaplani'
         else:
             await message.channel.send("Bledny argument numer 1. Powinna to byc cyfra od 1 do 6 odpowiadajaca "
@@ -58,29 +254,29 @@ async def on_message(message: discord.Message):
                                        "w tresci zadania.")
             return
         if arg3.lower() == 'wózki' or arg3.lower() == 'wozki':
-            m=6
+            m = 6
             jedn2 = 'Panes'
         elif arg3.lower() == 'noże' or arg3.lower() == 'noze':
-            m=7
+            m = 7
             jedn2 = 'Sileny'
         elif arg3.lower() == 'osły' or arg3.lower() == 'osly':
-            m=15
+            m = 15
             jedn2 = 'Fallofor'
         elif arg3.lower() == 'kozy':
-            m=3
+            m = 3
             jedn2 = 'Kanephoroi'
         elif arg3.lower() == 'leopardy':
-            m=8
+            m = 8
             jedn2 = 'Driady'
         elif arg3.lower() == 'pędzle' or arg3.lower() == 'pedzle':
-            m=20
+            m = 20
             jedn2 = 'Arcykaplani'
         else:
             await message.channel.send("Bledny argument numer 3. Powinien to byc wyraz w liczbie mnogiej w mianowniku, "
                                        "np. wozki, kozy, osly. Polskie znaki sa uznawane.")
             return
-        a = x*k
-        b = math.ceil(a/(m-1))
+        a = x * k
+        b = math.ceil(a / (m - 1))
         await message.channel.send(f"Do {msg} uzyj {a} {jedn1} i {b} {jedn2}")
 
     elif message.content.lower().startswith("ochr "):
@@ -115,7 +311,7 @@ async def on_message(message: discord.Message):
         args = msg.split()
         if len(args) == 2:
             try:
-                res = math.ceil(int(args[1])/21)
+                res = math.ceil(int(args[1]) / 21)
             except:
                 await message.channel.send("Przyjmuje tylko liczby.")
                 return
@@ -129,8 +325,8 @@ async def on_message(message: discord.Message):
             except:
                 await message.channel.send("Przyjmuje tylko liczby.")
                 return
-            res = math.ceil((ships*5 + units + free)/21)
-            await message.channel.send(f"Zbuduj jeszcze {res-ships} transow, zeby bylo ich lacznie {res}")
+            res = math.ceil((ships * 5 + units + free) / 21)
+            await message.channel.send(f"Zbuduj jeszcze {res - ships} transow, zeby bylo ich lacznie {res}")
 
         else:
             await message.channel.send("Nalezy podac tylko liczbe wolnych mieszkancow lub kolejno liczbe wolnych "
@@ -138,4 +334,44 @@ async def on_message(message: discord.Message):
             return
 
 
-bot.run('INSERT YOUR TOKEN HERE')
+def thread_discord():
+    safe_print("Discord thread active.")
+    client.run(bot_token)
+
+
+def get_skill_effect(skill, points):
+    return skill["multiplier"] * math.pow(points, skill["power"])
+
+
+def get_score_as_individual_sum(player_points, a):
+    keys = list(score_matrix[a].keys())
+    return base_score[a] + sum(get_skill_effect(score_matrix[a][skill], player_points[i]) for i, skill in enumerate(keys))
+
+
+def test_skill_distribution(skill_point_count):
+    best_dist = [{"skills": [], "score": -1}, {"skills": [], "score": -1}, {"skills": [], "score": -1},
+                {"skills": [], "score": -1}]
+    for s1 in range(skill_point_count + 1):
+        for s2 in range(skill_point_count - s1 + 1):
+            for s3 in range(skill_point_count - s1 - s2 + 1):
+                for i in range(0, 4):
+                    current_dist = {"skills": [s1, s2, s3], "score": get_score_as_individual_sum([s1, s2, s3], i)}
+                    if current_dist["score"] > best_dist[i]["score"]:
+                        best_dist[i] = current_dist
+
+    return best_dist
+
+
+def log_to_file(text, log_file='log.txt'):
+    current_datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    log_message = f'{current_datetime}: {text}\n'
+    with open(log_file, 'a') as f:
+        f.write(log_message)
+    f.close()
+
+
+print("Starting...")
+x = threading.Thread(target=thread_discord)
+x.start()
+safe_print("Starting server")
+app.run(port=8002, host="0.0.0.0")
